ca91426328eef750713134f75fd2c6c52fd5e9a0
xreider <xreider@yandex.ru>
2021-03-30 16:17:56 +1000

    got nav bar working

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
-import css from 'rollup-plugin-css-only'
+// import css from 'rollup-plugin-css-only'
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
@@ -37,7 +37,7 @@ export default {
 					hydratable: true
 				}
 			}),
-			css({output: 'static/extra.css'}),
+			// css({output: 'static/extra.css'}),
 			url({
 				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
 				publicPath: '/client/'
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,3 +1,49 @@
 <script>
+  let isOpen = false
   export let segment
+  function toggleNav() {
+    isOpen = !isOpen
+  }
 </script>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a href="/" class="navbar-item">
+      <img width="30" src="logo.png" alt="covid" />
+    </a>
+    <span
+      role="button"
+      class="navbar-burger"
+      class:is-active={isOpen}
+      aria-label="menu"
+      aria-expanded="false"
+      on:click={toggleNav}
+    >
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </span>
+  </div>
+  <div class="navbar-menu" class:is-active={isOpen}>
+    <div class="navbar-start">
+      <a href="/" class="navbar-item" class:is-active={segment === undefined}>
+        Home
+      </a>
+      <a
+        href="/about"
+        class="navbar-item"
+        class:is-active={segment === 'about'}
+      >
+        About
+      </a>
+    </div>
+  </div>
+</nav>
+
+<style>
+  @media (min-width: 769px) {
+    nav {
+      border-bottom: 1px solid gray;
+    }
+  }
+</style>
